7f36a42a810796abc2bdab44ea6c7eddfd82394f
shaanzie <ishaanlagwankar@gmail.com>
2019-10-10 20:27:20 +0530

    Updated Batsman Rank

--- IPLSPark/BatsmanRank.py
+++ IPLSPark/BatsmanRank.py
@@ -8,79 +8,52 @@ from pyspark.sql import SparkSession
 
 
 def computeContribs(urls, rank):
-    """Calculates URL contributions to the rank of other URLs."""
     num_urls = len(urls)
     for url in urls:
         yield (url, rank / num_urls)
 
 
 def parseNeighbors(urls):
-    """Parses a urls pair string into urls pair."""
     parts = re.split(',', urls)
     return parts[0], parts[1]
 
-def parseBat(urls):
+def parseAvg(urls):
     parts = re.split(',', urls)
-    return float(parts[2])/float(parts[3]), parts[0] + ":" + parts[1] 
-
+    return parts[0], float(int(parts[2]))/float(int(parts[3])) 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: pagerank <file> and optional <iterations>", file=sys.stderr)
+    if len(sys.argv) < 4:
+        print("Usage: pagerank <file> <iterations> <rank update rule>", file=sys.stderr)
         sys.exit(-1)
 
-    print("WARN: This is a naive implementation of PageRank and is given as an example!\n" +
-          "Please refer to PageRank implementation provided by graphx",
-          file=sys.stderr)
-
-    # Initialize the spark context.
     spark = SparkSession\
         .builder\
-        .appName("PythonPageRank")\
+        .appName("Task 1")\
         .getOrCreate()
 
-    # Loads in input file. It should be in format of:
-    #     URL         neighbor URL
-    #     URL         neighbor URL
-    #     URL         neighbor URL
-    #     ...
     lines = spark.read.text(sys.argv[1]).rdd.map(lambda r: r[0])
 
-    # Loads all URLs from input file and initialize their neighbors.
     links = lines.map(lambda urls: parseNeighbors(urls)).distinct().groupByKey().cache()
 
-    avgs = lines.map(lambda urls: parseBat(urls)).distinct().groupByKey().cache()
-    final_map = {}
-    for (link, rank) in avgs.collect():
-        for i in rank:
-         bat_guy = i.split(":")[0]
-         bowl_guy = i.split(":")[1]
+    ranks = lines.map(lambda string: parseAvg(string))\
+					.reduceByKey(add)\
+					.mapValues(lambda x: 1.0 if x < 1 else x)\
+					.cache()
 
-        #  print("batsman %s and bowler %s and the type is %s" % (bat_guy, bowl_guy, type(link)))
-         if(bowl_guy not in final_map):
-             final_map[bowl_guy] = link
-         else:
-             final_map[bowl_guy] += link
-    # Loads all URLs with other URL(s) link to from input file and initialize ranks of them to one.
-    
-    ranks = links.map(lambda url_neighbors: (url_neighbors[0], max(1, final_map[url_neighbors[0]])))
-
-    prev_lambda_list = [1]
     prev_lambda = 0
-    iter = 0
-
+    curr_lambda = ranks.max()[1]
+    itr = 0
     weight = int(sys.argv[3])
     weight = weight/100.0
 
-    # Calculates and updates URL ranks continuously using PageRank algorithm.
     if int(sys.argv[2]) != 0:
+
         for iteration in range(int(sys.argv[2])):
-            # Calculates URL contributions to the rank of other URLs.
+
             contribs = links.join(ranks).flatMap(
                 lambda url_urls_rank: computeContribs(url_urls_rank[1][0], url_urls_rank[1][1]))
 
-            # Re-calculates URL ranks based on neighbor contributions.
-            ranks = contribs.reduceByKey(add).mapValues(lambda rank: rank * weight + (1- weight))
+            ranks = contribs.reduceByKey(add).mapValues(lambda rank: rank * weight + (1-weight))
 
     else:
         iterations = 0
@@ -92,14 +65,17 @@ if __name__ == "__main__":
               lambda url_urls_rank: computeContribs(url_urls_rank[1][0], url_urls_rank[1][1]))
           prev_ranks = ranks
           ranks = contribs.reduceByKey(add).mapValues(lambda rank: rank * weight + (1-weight))
-          prev_ranks_new = list(prev_ranks.collect())
+       	  prev_ranks_new = list(prev_ranks.collect())
           sorted_prev = sorted(prev_ranks_new, key = lambda x: x[0])
        	  ranks_new = list(ranks.collect())
           sorted_pres = sorted(ranks_new, key = lambda x: x[0])    
           t1 = sorted_prev[0][1]
           t2 = sorted_pres[0][1]
-    # Collects all URL ranks and dump them to console.
-    ranks = ranks.sortBy(lambda x: x[1], False)
+
+    ranks = ranks.map(lambda x:(x[1],x[0]))\
+					.sortByKey(False)\
+					.map(lambda x:(x[1],x[0]))
+
     for (link, rank) in ranks.collect():
         print("%s,%s" % (link, rank))
 
