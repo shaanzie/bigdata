a5cd5578a015fc1f6fb5eee470210887f4e22ae3
Tejvi M <tejvimstays@gmail.com>
2019-10-25 19:21:52 +0530

    cleaned hashtags, outputs the top 20, should output 1

--- SparkStream/t1.py
+++ SparkStream/t1.py
@@ -22,11 +22,15 @@ def process_rdd(time, rdd):
 			row_rdd = rdd.map(lambda w: Row(tweetid=w[0], no_of_tweets=w[1]))
 			hashtags_df = sql_context.createDataFrame(row_rdd)
 			hashtags_df.registerTempTable("hashtags")
-			hashtag_counts_df = sql_context.sql("select tweetid, no_of_tweets from hashtags order by no_of_tweets desc limit 3")
+			hashtag_counts_df = sql_context.sql("select tweetid, no_of_tweets from hashtags order by no_of_tweets desc limit 20")
 			hashtag_counts_df.show()
 		except:
 			e = sys.exc_info()[0]
 			print("Error: %s" % e)
+
+def cleanData(x):
+  hashtags = x.split(",")
+  return hashtags
 
 def tmp(x):
 	return (x.split(';')[0],1)
@@ -35,24 +39,27 @@ conf=SparkConf()
 conf.setAppName("BigData")
 sc=SparkContext(conf=conf)
 
-ssc=StreamingContext(sc,2)
+ssc=StreamingContext(sc,0.001)
 ssc.checkpoint("/checkpoint_BIGDATA")
 
 dataStream=ssc.socketTextStream("localhost",9009)
 # dataStream.pprint()
 tweet=dataStream.map(tmp)
 # OR
-tweet=dataStream.map(lambda w:(w.split(';')[7],1))
-count=tweet.reduceByKey(lambda x,y:x+y)
+tweet=dataStream.map(lambda w:w.split(';')[7])
+tweet1=tweet.flatMap(lambda x: cleanData(x))
+tweet2=tweet1.map(lambda x: (x, 1))
+
+count=tweet2.reduceByKey(lambda x,y:x+y)
 count.pprint()
 
 #TO maintain state
-totalcount=tweet.updateStateByKey(aggregate_tweets_count)
+totalcount=tweet2.updateStateByKey(aggregate_tweets_count)
 totalcount.pprint()
 
 #To Perform operation on each RDD
 totalcount.foreachRDD(process_rdd)
 
 ssc.start()
-ssc.awaitTermination(2)
+ssc.awaitTermination(12)
 ssc.stop()
