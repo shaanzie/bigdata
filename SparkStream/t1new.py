895af49816d5968d5f3d0728835d1d3c51e673ae
Tejvi M <tejvimstays@gmail.com>
2019-10-29 16:23:23 +0530

    working code for task 1, both subtasks

--- SparkStream/t1new.py
+++ SparkStream/t1new.py
@@ -21,22 +21,25 @@ userSchema = StructType().add("1", "string") \
 		.add("10", "string") \
 		.add("11", "string") \
 		.add("12", "string") \
-		.add("13", "string") \
-		.add("14", "string")
+		.add("c13", "string") \
+		.add("c14", "string")
+
+#maxfilestrigger not necessary.
+#monitors stream directory
 csvDF = spark \
     .readStream \
+#    .option("maxFilesPerTrigger", 1)
     .option("sep", ";") \
     .schema(userSchema) \
-    .csv("hdfs://localhost:9000/A3/")
+    .csv("hdfs://localhost:9000/stream/")
 
 
 hashtags = csvDF.select("c8")
 words = hashtags.select(explode(split(hashtags.c8, ",")))
 words = words.withColumnRenamed("col", "hashtag")
+df1 = csvDF.select("11", "c13", "c14").withColumn("ratio", (csvDF.c13/ csvDF.c14))
 
 #if you dont specify a value for awaittermination, then this will run forever without giving a chance to the subsequent queries
-words.groupBy("hashtag").count().orderBy("count", ascending = False).writeStream.outputMode("complete").format("console").start().awaitTermination()
+words.groupBy("hashtag").count().orderBy("count", ascending = False).writeStream.outputMode("complete").format("console").start().awaitTermination(20)
 
-pop = csvDF.select("11", getRatio("13", "14").alias("ratio1"))
-pop = pop.groupBy("11").agg(F.max("ratio1"))
-pop = pop.orderBy("max(ratio1)", ascending = False).writeStream.outputMode("complete").format("console").start().awaitTermination(20)
+df1.groupBy("11").agg(F.max("ratio")).orderBy("max(ratio)", ascending = False).writeStream.outputMode("complete").format("console").start().awaitTermination(20)
